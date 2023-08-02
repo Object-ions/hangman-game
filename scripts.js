000a4c6f12662a858bbd865bddd5bc22a9c7694c
object-ions <moshikoatia@gmail.com>
2023-08-01 17:56:48 -0700

    add displayWord

--- scripts.js
+++ scripts.js
@@ -5,3 +5,32 @@ const playAgainBtn = document.getElementById('play-again');
 const popup = document.getElementById('popup-container');
 const notification = document.getElementById('notification-container');
 const finalMessage = document.getElementById('final-message');
+
+//Get figure parts
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+console.log(selectedWord);
+
+const correctLetters = [];
+const wrongLetters = [];
+
+//Show the hidden word
+function dispalyWord () {
+  words.innerHtml = `
+  ${selectedWord
+    .split('')
+    .map(letter => `
+    <span class = "letter">
+    ${correctLetters.includes(letter) ? letter : ''}
+    </span>
+    `
+    )
+    .join('')
+  }
+  `;
+}
+
+dispalyWord();
